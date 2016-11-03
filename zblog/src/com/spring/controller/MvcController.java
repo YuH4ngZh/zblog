4012c6848bb3dac96daf64406a337997b5090e67
YuH4ngZh <359294760@qq.com>
2016-11-03 23:25:28 +0800

    first

---
+++ zblog/src/com/spring/controller/MvcController.java
@@ -0,0 +1,19 @@
+package com.spring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/mvc")
+public class MvcController {
+ 
+    @RequestMapping("/hello")
+    public String hello(){ 
+    		return "hello";
+    }
+    
+}
